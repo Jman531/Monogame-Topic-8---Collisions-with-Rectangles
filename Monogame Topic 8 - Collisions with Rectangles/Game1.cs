1c4271457e3b5e360f0aeafe5d1aeffd8278fba1
Jack Gallagher <339690067@GoTVDSB.ca>
2024-11-18 09:40:10 -0500

    Class ended

--- Monogame Topic 8 - Collisions with Rectangles/Game1.cs
+++ Monogame Topic 8 - Collisions with Rectangles/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Monogame_Topic_8___Collisions_with_Rectangles
 {
@@ -8,6 +9,25 @@ namespace Monogame_Topic_8___Collisions_with_Rectangles
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        int pacSpeed;
+
+        Rectangle window;
+
+        KeyboardState keyboardState;
+        MouseState mouseState;
+
+        Texture2D pacLeftTexture, pacRightTexture, pacUpTexture, pacDownTexture, currentPacTexture;
+        Rectangle pacRect;
+
+        Texture2D exitTexture;
+        Rectangle exitRect;
+
+        Texture2D barrierTexture;
+        Rectangle barrierRect1, barrierRect2;
+
+        Texture2D coinTexture;
+        List<Rectangle> coins;
 
         public Game1()
         {
@@ -19,8 +39,26 @@ namespace Monogame_Topic_8___Collisions_with_Rectangles
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 500);
+
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.ApplyChanges();
 
             base.Initialize();
+            pacSpeed = 3;
+            pacRect = new Rectangle(10, 10, 60, 60);
+
+            barrierRect1 = new Rectangle(0, 250, 350, 75);
+            barrierRect2 = new Rectangle(450, 250, 350, 75);
+
+            coins = new List<Rectangle>();
+            coins.Add(new Rectangle(400, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(475, 50, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(200, 300, coinTexture.Width, coinTexture.Height));
+            coins.Add(new Rectangle(400, 300, coinTexture.Width, coinTexture.Height));
+
+            exitRect = new Rectangle(700, 380, 100, 100);
         }
 
         protected override void LoadContent()
@@ -28,6 +66,15 @@ namespace Monogame_Topic_8___Collisions_with_Rectangles
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            currentPacTexture = pacRightTexture;
+
+            exitTexture = Content.Load<Texture2D>("hobbit_door");
+            barrierTexture = Content.Load<Texture2D>("rock_barrier");
+            coinTexture = Content.Load<Texture2D>("coin");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +83,52 @@ namespace Monogame_Topic_8___Collisions_with_Rectangles
                 Exit();
 
             // TODO: Add your update logic here
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacRect.X -= pacSpeed;
+                currentPacTexture = pacLeftTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacRect.X += pacSpeed;
+                currentPacTexture = pacRightTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacRect.Y -= pacSpeed;
+                currentPacTexture = pacUpTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacRect.Y += pacSpeed;
+                currentPacTexture = pacDownTexture;
+            }
+
+            for (int i = 0; i < coins.Count; i++)
+            {
+                if (pacRect.Intersects(coins[i]))
+                {
+                    coins.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (pacRect.Intersects(barrierRect1))
+            {
+                pacRect.X = barrierRect1.Left - pacRect.Width;
+            }
+            if (pacRect.Intersects(barrierRect2))
+            {
+                pacRect.X = barrierRect2.Left - pacRect.Width;
+            }
+            if (exitRect.Contains(pacRect))
+                Exit();
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+                if (exitRect.Contains(mouseState.X, mouseState.Y))
+                    Exit();
 
             base.Update(gameTime);
         }
@@ -45,6 +138,16 @@ namespace Monogame_Topic_8___Collisions_with_Rectangles
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(barrierTexture, barrierRect1, Color.White);
+            _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+            _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+            foreach (Rectangle coin in coins)
+                _spriteBatch.Draw(coinTexture, coin, Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
